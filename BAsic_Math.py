00dc8e8f4c2df1ca6199eed011a2d311a1ff5d56
Jesso <jessothomas0@gmail.com>
2021-05-14 11:52:15 -0400

    Add files via upload

---
+++ BAsic_Math.py
@@ -0,0 +1,22 @@
+
+
+import math
+
+import os
+
+
+x = input("Please enter a first  number :\n")
+y = input("Please enter a second number :\n")
+
+print("The sum \n:",x+y)
+print("\t")
+
+
+print("The difference \n:",x-y)
+print("\t")
+
+
+print("The divison \n:",x/y)
+print("\t")
+
+print("The product \n:",x*y)
